82b4d7474ade3dced332531be10bf83d85346524
Kim Schaefer <kim.s.schaefer@gmail.com>
2021-05-15 10:01:31 -0600

    Installed dependencies and initialized app

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 3000;
+  port = 3003;
 }
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, './client/dist')));
 app.use(cors());
 
 if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () => {
-    console.log('SDC is listening on port 3000');
+    console.log('WMM is listening on port 3003');
   });
 }
