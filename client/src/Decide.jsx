759f8fbe5895d80bd92d52c3e413f2f9b6cd9b1c
Kim Schaefer <kim.s.schaefer@gmail.com>
2021-05-17 19:34:27 -0600

    CSS styling for decide page done

--- client/src/Decide.jsx
+++ client/src/Decide.jsx
@@ -4,12 +4,28 @@ import ReactDOM from 'react-dom';
 const Decide = () => {
   return (
     <div>
-      <div id="top">
-        <button>Option A</button>
+      <div id="top-div">
+        <button className="option-button" id="option-A">Option A</button>
       </div>
 
-      <div id="bottom">
-        <button>Option B</button>
+      <div id="bottom-div">
+        <button className="option-button" id="option-B">Option B</button>
+      </div>
+
+      <div id="top-div">
+        <button className="option-button" id="option-C">Option C</button>
+      </div>
+
+      <div id="bottom-div">
+        <button className="option-button" id="option-D">Option D</button>
+      </div>
+
+      <div id="top-div">
+        <button className="option-button" id="option-E">Option E</button>
+      </div>
+
+      <div id="bottom-div">
+        <button className="option-button" id="option-F">Option F</button>
       </div>
     </div>
   )
